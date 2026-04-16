78d0f079af02b17b4fd119a9067e67dcffb263a8
Marcus Viden <marcus.viden@ub.uu.se>
2026-04-16 18:16:43 +0200

    fixes to stabilize tests

--- test.sh
+++ test.sh
@@ -1,111 +1,121 @@
 #!/bin/bash
+
 PROFILE="$1"
 PROFILE_FILE="./profiles/${PROFILE}.env"
-TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+TIMESTAMP=$(date +%Y%m%d-[%H%M%S])
 
-#Output
-RESULTS_DIR="./results/${PROFILE}"
-REPORTS_DIR="./reports/${PROFILE}"
-LOG_DIR="./logs/${PROFILE}"
+# Output
+OUTPUT_DIR="./output/${PROFILE}/${TIMESTAMP}"
+
+RESULTS_DIR="${OUTPUT_DIR}/results"
+REPORTS_DIR="${OUTPUT_DIR}/report"
+LOG_DIR="${OUTPUT_DIR}/logs"
+FAILED_RESPONSE_DIR="${OUTPUT_DIR}/failed"
 
 start_test() {
-	load_test_profile
-	validate_required_vars
-	create_output_directories
-	print_test_info
-	run_jmeter
+    load_test_profile
+    validate_required_vars
+    create_output_directories
+    print_test_info
+    run_jmeter
 }
 
 load_test_profile() {
-	validate_profile_exists
+    validate_profile_exists
 
-	set -a
-	source "$PROFILE_FILE"
-	set +a
+    set -a
+    source "$PROFILE_FILE"
+    set +a
 
     # Load local env (for dev use only)
-	if [ -f "./env.local" ]; then
-		set -a
-		source ./env.local
-		set +a
-	fi
+    if [ -f "./env.local" ]; then
+        set -a
+        source ./env.local
+        set +a
+    fi
 }
 
 validate_profile_exists() {
-	if [ ! -f "$PROFILE_FILE" ]; then
-		echo "Unknown profile: $PROFILE"
-		echo ""
-		list_profiles
-		exit 1
-	fi
+    if [ ! -f "$PROFILE_FILE" ]; then
+        echo "Unknown profile: $PROFILE"
+        echo ""
+        list_profiles
+        exit 1
+    fi
 }
 
 list_profiles() {
-	echo "Available test profiles:"
+    echo "Available test profiles:"
 
-	shopt -s nullglob
-	files=(./profiles/*.env)
+    shopt -s nullglob
+    files=(./profiles/*.env)
 
-	if [ ${#files[@]} -eq 0 ]; then
-		echo " (none found)"
-		return
-	fi
+    if [ ${#files[@]} -eq 0 ]; then
+        echo " (none found)"
+        return
+    fi
 
-	for file in "${files[@]}"; do
-		name=$(basename "$file" .env)
-		echo " - $name"
-	done
+    for file in "${files[@]}"; do
+        name=$(basename "$file" .env)
+        echo " - $name"
+    done
 }
 
 validate_required_vars() {
-	: "${HOST:?}"
-	: "${PORT:?}"
-	: "${PROTOCOL:?}"
-	: "${USER_LOAD:?}"
-	: "${RAMP_SECONDS:?}"
-	: "${DURATION_SECONDS:?}"
-	: "${PACING_BASE_MILLIS:?}"
-	: "${PACING_VARIANCE_MILLIS:?}"
-	: "${ADMIN_USER:?}"
-	: "${ADMIN_APPTOKEN:?}"
+    : "${HOST:?}"
+    : "${PORT:?}"
+    : "${PROTOCOL:?}"
+    : "${USER_LOAD:?}"
+    : "${RAMP_SECONDS:?}"
+    : "${DURATION_SECONDS:?}"
+    : "${PACING_BASE_MILLIS:?}"
+    : "${PACING_VARIANCE_MILLIS:?}"
+    : "${ADMIN_USER:?}"
+    : "${ADMIN_APPTOKEN:?}"
 }
 
 create_output_directories() {
-	mkdir -p "$RESULTS_DIR" "$REPORTS_DIR" "$LOG_DIR"
+    mkdir -p \
+        "$RESULTS_DIR" \
+        "$REPORTS_DIR" \
+        "$LOG_DIR" \
+        "$FAILED_RESPONSE_DIR"
 }
 
 print_test_info() {
-	echo ""
-	echo "============================================================"
-	echo " Running load profile: ${PROFILE}"
-	echo "============================================================"
-	echo "Target        : ${PROTOCOL}://${HOST}:${PORT}"
-    echo "Admin user    : ${ADMIN_USER}"
-	echo "User load     : ${USER_LOAD}"
-	echo "Ramp          : ${RAMP_SECONDS}s"
-	echo "Duration      : ${DURATION_SECONDS}s"
-    echo "Pacing base   : ${PACING_BASE_MILLIS}"
-    echo "Pacing vari   : ${PACING_VARIANCE_MILLIS}"
-	echo "============================================================"
-	echo ""
+    echo ""
+    echo "============================================================"
+    echo " Running load profile: ${PROFILE}"
+    echo "============================================================"
+    echo " Target        : ${PROTOCOL}://${HOST}:${PORT}"
+    echo " Admin user    : ${ADMIN_USER}"
+    echo " User load     : ${USER_LOAD}"
+    echo " Ramp          : ${RAMP_SECONDS}s"
+    echo " Duration      : ${DURATION_SECONDS}s"
+    echo " Pacing base   : ${PACING_BASE_MILLIS}ms"
+    echo " Pacing vari   : ${PACING_VARIANCE_MILLIS}ms"
+    echo " Output dir    : ${OUTPUT_DIR}"
+    echo "============================================================"
+    echo ""
 }
 
 run_jmeter() {
-	jmeter -n \
-		-t ./tests/crud-load.jmx \
-		-JPROTOCOL="$PROTOCOL" \
-		-JHOST="$HOST" \
-		-JPORT="$PORT" \
-		-JUSER_LOAD="$USER_LOAD" \
-		-JRAMP_SECONDS="$RAMP_SECONDS" \
-		-JDURATION_SECONDS="$DURATION_SECONDS" \
-		-JPACING_BASE_MILLIS="$PACING_BASE_MILLIS" \
-		-JPACING_VARIANCE_MILLIS="$PACING_VARIANCE_MILLIS" \
-		-JADMIN_USER="$ADMIN_USER" \
-		-JADMIN_APPTOKEN="$ADMIN_APPTOKEN" \
-		-l "$RESULTS_DIR/${PROFILE}-${TIMESTAMP}.jtl" \
-		-j "$LOG_DIR/${PROFILE}-${TIMESTAMP}.log" \
-		-e -o "$REPORTS_DIR/${PROFILE}-${TIMESTAMP}"
+    jmeter -n \
+        -t ./tests/crud-load.jmx \
+        -JPROTOCOL="$PROTOCOL" \
+        -JHOST="$HOST" \
+        -JPORT="$PORT" \
+        -JUSER_LOAD="$USER_LOAD" \
+        -JRAMP_SECONDS="$RAMP_SECONDS" \
+        -JDURATION_SECONDS="$DURATION_SECONDS" \
+        -JPACING_BASE_MILLIS="$PACING_BASE_MILLIS" \
+        -JPACING_VARIANCE_MILLIS="$PACING_VARIANCE_MILLIS" \
+        -JADMIN_USER="$ADMIN_USER" \
+        -JADMIN_APPTOKEN="$ADMIN_APPTOKEN" \
+        -JFAILED_RESPONSE_DIR="$FAILED_RESPONSE_DIR" \
+        -l "$RESULTS_DIR/results.jtl" \
+        -j "$LOG_DIR/jmeter.log" \
+        -e -o "$REPORTS_DIR"
 }
 
 start_test
